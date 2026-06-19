f9098047b372d40945f27bd62ed8be4f68823553
Jonathan <jonathan@ritua.app>
2026-06-19 08:41:46 -0300

    build: bump version to 1.0.1 for the usage-refresh fix release

--- NightConductor/build-app.sh
+++ NightConductor/build-app.sh
@@ -21,8 +21,8 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
     <key>CFBundleName</key><string>Night Conductor</string>
     <key>CFBundleDisplayName</key><string>Night Conductor</string>
     <key>CFBundlePackageType</key><string>APPL</string>
-    <key>CFBundleShortVersionString</key><string>1.0.0</string>
-    <key>CFBundleVersion</key><string>1</string>
+    <key>CFBundleShortVersionString</key><string>1.0.1</string>
+    <key>CFBundleVersion</key><string>2</string>
     <key>CFBundleIconFile</key><string>AppIcon</string>
     <key>LSMinimumSystemVersion</key><string>15.0</string>
     <key>LSUIElement</key><true/>
